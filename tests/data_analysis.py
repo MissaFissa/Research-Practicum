084dce620827fb5ca7c1ba8636c8c9ca1b2bbb31
MissaFissa <meho11@outlook.com>
2025-01-13 19:08:37 +0100

    Converted data to float64

--- tests/data_analysis.py
+++ tests/data_analysis.py
@@ -4,8 +4,15 @@ import pandas as pd
 from pathlib import Path
 
 cwd = Path.cwd()
-df = pd.read_csv(f'{cwd}/data_csv/12_measurements_400mu.csv', sep=';', skiprows=[0,1,2,3,4], header=[0])
+
+df = pd.read_csv(f'{cwd}/data_csv/12_measurements_400mu.csv', sep=';', skiprows=[0,1,2,3,4], header=[0], decimal=',')
 df_dict = dict(enumerate(df))
 
-for value in df_dict.values():
-    print(value)
+fig, ax = plt.subplots(figsize=(10, 8))
+
+for i in range(1,13):
+
+    ax.plot(df[df_dict[0]], df[df_dict[i]])
+
+ax.grid(True, which = 'minor', lw = .5, alpha = .2)
+plt.show()
