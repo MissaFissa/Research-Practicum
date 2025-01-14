7c4cc2f084bb11d47b1ff0b521dda22dd20c4f94
MissaFissa <meho11@outlook.com>
2025-01-14 16:56:51 +0100

    trying to make code more readable

--- tests/analysis.py
+++ tests/analysis.py
@@ -1,7 +1,8 @@
+import numpy as np
 import data
 
 NA = 0.39
-n_IL = 1.33
+n_water = 1.33
 n_air = 1.0
 
 df200 = data.df_200
@@ -10,12 +11,8 @@ dict200 = data.dict_200
 dict400 = data.dict_400
 info200 = data.info_200
 info400 = data.info_400
-
 t200 = list(info200.iloc[0])
 t400 = list(info400.iloc[0])
-
-print(t200)
-print(t400)
 
 wavelengths = df400[dict400[0]]
 
@@ -55,10 +52,47 @@ def eta_lim(n_sample):
 
 def refraction(R_ref, I_sample, I_sample_back, I_ref, I_ref_back):
 
-    return R_ref * ((I_sample - I_sample_back) / I_ref - I_ref_back)
+    return R_ref * ((I_sample - I_sample_back) / (I_ref - I_ref_back))
 
 eta_lim_air = eta_lim(n_air)
-eta_lim_IL = eta_lim(n_IL)
+eta_lim_water = eta_lim(n_water)
 
-# for i in range(1,13):
+def ratio_time(times, a, b):
 
+    t1 = times[a - 1] 
+    t2 = times[b - 1]
+
+    if t1 > t2:
+
+        return t1 / t2
+    
+    else:
+
+        return t2 / t1
+  
+def corrections(ratio, a):
+
+    return np.array([i / ratio for i in a])
+
+cor_fresnel_400_280ms_15deg = corrections(ratio_time(t400, 1, 5), fresnel_400_280ms_15deg)
+cor_fresnel_400_280ms_15deg_dark = corrections(ratio_time(t400, 2, 6), fresnel_400_280ms_15deg_dark)
+cor_R_IL_400_15deg = refraction(eta_lim_air, IL_400_6ms_15deg, IL_400_6ms_15deg_dark, cor_fresnel_400_280ms_15deg, cor_fresnel_400_280ms_15deg_dark)
+
+R_IL_400_0deg = refraction(eta_lim_air, IL_400_6ms_0deg, IL_400_6ms_0deg_dark, fresnel_400_6ms_0deg, fresnel_400_6ms_0deg_dark)
+R_IL_400_15deg = refraction(eta_lim_air, IL_400_6ms_15deg, IL_400_6ms_15deg_dark, fresnel_400_280ms_15deg, fresnel_400_280ms_15deg_dark)
+
+R_IL_200_0deg = refraction(eta_lim_air, IL_200_5ms_0deg, IL_200_5ms_0deg_dark, fresnel_200_5ms_0deg, fresnel_200_5ms_0deg_dark)
+R_IL_200_15deg = refraction(eta_lim_air, IL_200_5ms_15deg, IL_200_5ms_15deg_dark, fresnel_200_1400ms_15deg, fresnel_200_1400ms_15deg_dark)
+
+cor_water_400_107ms_0deg = corrections(ratio_time(t400, 9, 5), water_400_107ms_0deg)
+cor_water_400_107ms_0deg_dark = corrections(ratio_time(t400, 10, 6), water_400_107ms_0deg_dark)
+cor_R_IL_400_0deg_water = refraction(eta_lim_water, IL_400_6ms_0deg, IL_400_6ms_0deg_dark, cor_water_400_107ms_0deg, cor_water_400_107ms_0deg_dark)
+
+R_IL_400_0deg_water = refraction(eta_lim_water, IL_400_6ms_0deg, IL_400_6ms_0deg_dark, water_400_107ms_0deg, water_400_107ms_0deg_dark)
+R_IL_400_15deg_water = refraction(eta_lim_water, IL_400_6ms_15deg, IL_400_6ms_15deg_dark, water_400_2000ms_15deg, water_400_2000ms_15deg_dark)
+
+print(cor_R_IL_400_0deg_water[0:10])
+print(R_IL_400_0deg_water[0:10])
+
+R_IL_200_0deg_water = refraction(eta_lim_water, IL_200_5ms_0deg, IL_200_5ms_0deg_dark, water_200_73ms_0deg, water_200_73ms_0deg_dark)
+R_IL_200_15deg_water = refraction(eta_lim_water, IL_200_5ms_15deg, IL_200_5ms_15deg_dark, water_200_1600ms_15deg, water_200_1600ms_15deg_dark)
--- tests/plots.py
+++ tests/plots.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import data
+import analysis
 
-fig, axs = plt.subplots(2, 1, figsize=(10, 8))
+fig1, axs1 = plt.subplots(2, 1, figsize=(10, 8))
 
 for i in range(1,13):
 
-    axs[0].plot(data.df_400[data.dict_400[0]], data.df_400[data.dict_400[i]], label=f'{i}')
-    axs[1].plot(data.df_200[data.dict_200[0]], data.df_200[data.dict_200[i]], label=f'{i}')
+    axs1[0].plot(data.df_400[data.dict_400[0]], data.df_400[data.dict_400[i]], label=f'{i}')
+    axs1[1].plot(data.df_200[data.dict_200[0]], data.df_200[data.dict_200[i]], label=f'{i}')
 
 major_ticks_x = np.arange(150, 1150, 50)
 minor_ticks_x = np.arange(150, 1150, 10)
 major_ticks_y = np.arange(0, 75000, 5000)
 minor_ticks_y = np.arange(0, 75000, 1000)
 
-for ax in axs:
+for ax in axs1:
 
     ax.set_xlabel('Wavelength [nm]')
     ax.set_ylabel('Scope [ADC Counts]')
@@ -31,10 +32,62 @@ for ax in axs:
     ax.set_ylim(0, 70000)
     ax.legend()
 
-axs[0].set_title('400$\\mu$ fiber')
-axs[1].set_title('200$\\mu$ fiber')
+axs1[0].set_title('400$\\mu$ fiber')
+axs1[1].set_title('200$\\mu$ fiber')
 
 plt.tight_layout()
 plt.margins(0)
+
+fig2, axs2 = plt.subplots(2, 2, figsize=(10, 8))
+
+axs2[0,0].plot(analysis.wavelengths, analysis.R_IL_400_0deg)
+axs2[0,1].plot(analysis.wavelengths, analysis.IL_400_6ms_0deg)
+
+axs2[1,0].plot(analysis.wavelengths, analysis.R_IL_200_0deg)
+axs2[1,1].plot(analysis.wavelengths, analysis.IL_200_5ms_0deg)
+
+plt.setp(axs2[0,0], xlim=(400,1100), ylim=(0,0.3))
+plt.setp(axs2[1,0], xlim=(400,1100), ylim=(0,0.3))
+
+
+
+
+fig3, axs3 = plt.subplots(2, 2, figsize=(10, 8))
+
+axs3[0,0].plot(analysis.wavelengths, analysis.R_IL_400_0deg_water)
+axs3[0,1].plot(analysis.wavelengths, analysis.R_IL_400_15deg_water)
+
+axs3[1,0].plot(analysis.wavelengths, analysis.R_IL_400_15deg_water)
+axs3[1,1].plot(analysis.wavelengths, analysis.R_IL_200_15deg_water)
+
+plt.setp(axs3, xlim=(400,1100), ylim=(0,0.3))
+
+fig4 = plt.figure()
+gs = fig4.add_gridspec(2, 2, hspace=0, wspace=0)
+axs4 = gs.subplots(sharex=True, sharey=True)
+
+axs4[0,0].plot(analysis.wavelengths, analysis.R_IL_400_0deg_water)
+axs4[0,1].plot(analysis.wavelengths, analysis.cor_R_IL_400_0deg_water)
+
+# axs4[0,1].plot(analysis.wavelengths, analysis.R_IL_400_15deg_water)
+
+axs4[1,0].plot(analysis.wavelengths, analysis.R_IL_400_15deg_water)
+axs4[1,1].plot(analysis.wavelengths, analysis.R_IL_200_15deg_water)
+
+# plt.setp(axs4, xlim=(400,1100), ylim=(0,0.3))
+
+fig5, axs5 = plt.subplots(2, 2, figsize=(10, 8))
+
+axs5[0,0].plot(analysis.wavelengths, analysis.fresnel_400_6ms_0deg)
+axs5[0,1].plot(analysis.wavelengths, analysis.IL_400_6ms_0deg)
+
+axs5[1,0].plot(analysis.wavelengths, analysis.fresnel_200_5ms_0deg)
+axs5[1,1].plot(analysis.wavelengths, analysis.IL_200_5ms_0deg)
+
+# plt.close(fig1)
+plt.close(fig2)
+plt.close(fig3)
+plt.close(fig4)
+# plt.close(fig5)
 
 plt.show()
