d1742dc52848ab5e13198d40900aac852d723646
MissaFissa <meho11@outlook.com>
2025-01-16 18:40:17 +0100

    corrected lots of data analysis

--- tests/data_gathering.py
+++ tests/data_gathering.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+import numpy as np
 
 cwd = Path.cwd()
 
@@ -7,8 +8,10 @@ class DataGathering:
 
     def __init__(self):
         
-        self.df_water= pd.read_csv(f'{cwd}/literature_data/Hale.csv', sep=',')
+        self.df_water = pd.read_csv(f'{cwd}/literature_data/Hale.csv', sep=',')
         self.dict_water = dict(enumerate(self.df_water))
+        self.wavelengths_water = self.df_water[self.dict_water[0]].astype(float)
+        self.indices_water = self.df_water[self.dict_water[1]].astype(float)
 
         self.df_400 = pd.read_csv(f'{cwd}/data/data_400mu_csv/12_measurements_400mu.csv', sep=';', skiprows=[0,1,2,3,4], header=[0], decimal=',')
         self.info_400 = pd.read_csv(f'{cwd}/data/data_400mu_csv/12_measurements_400mu.csv', sep=';', index_col=0, nrows=2, skiprows=[1,4], decimal=',')
@@ -18,46 +21,33 @@ class DataGathering:
         self.info_200 = pd.read_csv(f'{cwd}/data/data_200mu_csv/12_measurements_200mu.csv', sep=';', index_col=0, nrows=2, skiprows=[1,4], decimal=',')
         self.dict_200 = dict(enumerate(self.df_200))
 
-        self.t_200 = list(self.info_200.iloc[0])
-        self.t_400 = list(self.info_400.iloc[0])
+        self.t_200 = list(dict.fromkeys(self.info_200.iloc[0]))
+        self.t_400 = list(dict.fromkeys(self.info_400.iloc[0]))
+
+        self.index_wl400nm = 451
         self.wavelengths = self.df_400[self.dict_400[0]]
-        self.wavelengths_400nm = self.df_400[self.dict_400[0]][400:]
+        self.wavelengths_400nm = self.df_400[self.dict_400[0]][self.index_wl400nm:]
         self.n_eff_400_15deg = []
         self.n_eff_400_0deg = []
         self.n_eff_200_15deg = []
         self.n_eff_200_0deg = []
-        self.n_eff_400_unscaled_15deg = []
-        self.n_eff_400_unscaled_0deg = []
-        self.n_eff_200_unscaled_15deg = []
-        self.n_eff_200_unscaled_0deg = []
         self.NA = 0.39
         self.n_water = 1.33
         self.n_air = 1.0
         self.t_reference = 6
-        self.unscaled_400 = []
-        self.unscaled_400_light = []
-        self.unscaled_400_dark = []
-        self.unscaled_200 = []
-        self.unscaled_200_light = []
-        self.unscaled_200_dark = []
         self.scaled_400 = []
-        self.scaled_400_light = []
-        self.scaled_400_dark = []
         self.scaled_200 = []
-        self.scaled_200_light = []
-        self.scaled_200_dark = []
         self.dark_400 = []
         self.dark_200 = []
         self.good_400 = []
         self.good_200 = []
         self.corrected_400 = []
         self.corrected_200 = []
-
-    def water_indices(self):
-
-        for i in self.df_water[self.dict_water[0]][0:1]:
-
-            print(i * 1000)
+        self.water_indices = []
+        self.B1 = 0.75831
+        self.C1 = 0.01007
+        self.B2 = 0.08495
+        self.C2 = 8.91377
 
     def background_correction(self):
 
@@ -81,7 +71,7 @@ class DataGathering:
             corrected_400_temp = []
             corrected_200_temp = []
 
-            for j in range(len(self.df_400)):
+            for j in range(self.index_wl400nm, len(self.df_400)):
 
                 corrected_400 = self.df_400[self.good_400[i]][j] - self.df_400[self.dark_400[i]][j]
                 corrected_200 = self.df_200[self.good_200[i]][j] - self.df_200[self.dark_200[i]][j]
@@ -92,108 +82,55 @@ class DataGathering:
             self.corrected_400.append(corrected_400_temp)
             self.corrected_200.append(corrected_200_temp)
 
+        self.corrected_400_array = np.array(self.corrected_400)
+        self.corrected_200_array = np.array(self.corrected_200)
+
     def scale(self):
 
-        for i in range(1,13):
+        for i in range(len(self.corrected_400)):
 
-            self.unscaled_400.append(self.df_400[self.dict_400[i]])
-            self.unscaled_200.append(self.df_200[self.dict_200[i]])
-                    
-            scale_factor_400 = self.t_reference / self.t_400[i-1]
-            scale_factor_200 = self.t_reference / self.t_200[i-1]
+            scale_factor_400 = self.t_reference / self.t_400[i]
+            scale_factor_200 = self.t_reference / self.t_200[i]
             
-            self.scaled_400.append(scale_factor_400 * self.unscaled_400[i-1])
-            self.scaled_200.append(scale_factor_200 * self.unscaled_200[i-1])
+            self.scaled_400.append(scale_factor_400 * self.corrected_400_array[i])
+            self.scaled_200.append(scale_factor_200 * self.corrected_200_array[i])
 
-            if (i - 1) % 2 == 0:
-
-                self.unscaled_400_light.append(self.unscaled_400[i-1])
-                self.unscaled_200_light.append(self.unscaled_200[i-1])
-
-                self.scaled_200_light.append(self.scaled_200[i-1])
-                self.scaled_400_light.append(self.scaled_400[i-1])
-
-            else: 
-
-                self.unscaled_400_dark.append(self.unscaled_400[i-1])
-                self.unscaled_200_dark.append(self.unscaled_200[i-1])
-                
-                self.scaled_200_dark.append(self.scaled_200[i-1])
-                self.scaled_400_dark.append(self.scaled_400[i-1])
-
-    # def scale(self):
-
-    #     for i in range(1,13):
-
-    #         self.unscaled_400.append(self.df_400[self.dict_400[i]])
-    #         self.unscaled_200.append(self.df_200[self.dict_200[i]])
-                    
-    #         scale_factor_400 = self.t_reference / self.t_400[i-1]
-    #         scale_factor_200 = self.t_reference / self.t_200[i-1]
+    def sellmeier(self):
+        
+        for wl in self.wavelengths_400nm / 1000:
             
-    #         self.scaled_400.append(scale_factor_400 * self.unscaled_400[i-1])
-    #         self.scaled_200.append(scale_factor_200 * self.unscaled_200[i-1])
-
-    #         if (i - 1) % 2 == 0:
-
-    #             self.unscaled_400_light.append(self.unscaled_400[i-1])
-    #             self.unscaled_200_light.append(self.unscaled_200[i-1])
-
-    #             self.scaled_200_light.append(self.scaled_200[i-1])
-    #             self.scaled_400_light.append(self.scaled_400[i-1])
-
-    #         else: 
-
-    #             self.unscaled_400_dark.append(self.unscaled_400[i-1])
-    #             self.unscaled_200_dark.append(self.unscaled_200[i-1])
-                
-    #             self.scaled_200_dark.append(self.scaled_200[i-1])
-    #             self.scaled_400_dark.append(self.scaled_400[i-1])
+            n = (1 + ((self.B1 * wl ** 2) / (wl ** 2 - self.C1)) + ((self.B2 * wl ** 2) / (wl ** 2 - self.C2))) ** 0.5
+            
+            self.water_indices.append(n)
 
     def effective_index(self):
 
         b = self.n_air
         c = self.n_water
 
-        for i in range(400,len(self.df_400)):
+        for i in range(len(self.scaled_400[0])):
+            
+            # c = self.water_indices[i]
 
-            a_400_15_deg = ((self.scaled_400_light[0][i] - self.scaled_400_dark[0][i]) / (self.scaled_400_light[5][i] - self.scaled_400_dark[5][i])) ** 0.5
-            a_400_0_deg = ((self.scaled_400_light[1][i] - self.scaled_400_dark[1][i]) / (self.scaled_400_light[4][i] - self.scaled_400_dark[4][i])) ** 0.5
-
+            a_400_15_deg = (self.scaled_400[0][i] / self.scaled_400[5][i]) ** 0.5
+            a_400_0_deg = (self.scaled_400[1][i] / self.scaled_400[4][i]) ** 0.5
             n_eff_400_15deg = ((-1 * (c - b + a_400_15_deg * (c - b)) - ((c - b + a_400_15_deg * (c - b)) ** 2 + (4 * b * c * ((1 - a_400_15_deg) ** 2))) ** 0.5) / (2 * (1 - a_400_15_deg)))
             n_eff_400_0deg = ((-1 * (c - b + a_400_0_deg * (c - b)) - ((c - b + a_400_0_deg * (c - b)) ** 2 + (4 * b * c * ((1 - a_400_0_deg) ** 2))) ** 0.5) / (2 * (1 - a_400_0_deg)))
 
             self.n_eff_400_15deg.append(n_eff_400_15deg)
             self.n_eff_400_0deg.append(n_eff_400_0deg)
 
-            a_200_15_deg = ((self.scaled_200_light[0][i] - self.scaled_200_dark[0][i]) / (self.scaled_200_light[5][i] - self.scaled_200_dark[5][i])) ** 0.5
-            a_200_0_deg = ((self.scaled_200_light[1][i] - self.scaled_200_dark[1][i]) / (self.scaled_200_light[4][i] - self.scaled_200_dark[4][i])) ** 0.5
+            a_200_15_deg = (self.scaled_200[0][i] / self.scaled_200[5][i]) ** 0.5
+            a_200_0_deg = (self.scaled_200[1][i] / self.scaled_200[4][i]) ** 0.5
 
             n_eff_200_15deg = ((-1 * (c - b + a_200_15_deg * (c - b)) - ((c - b + a_200_15_deg * (c - b)) ** 2 + (4 * b * c * ((1 - a_200_15_deg) ** 2))) ** 0.5) / (2 * (1 - a_200_15_deg)))
             n_eff_200_0deg = ((-1 * (c - b + a_200_0_deg * (c - b)) - ((c - b + a_200_0_deg * (c - b)) ** 2 + (4 * b * c * ((1 - a_200_0_deg) ** 2))) ** 0.5) / (2 * (1 - a_200_0_deg)))
 
             self.n_eff_200_15deg.append(n_eff_200_15deg)
             self.n_eff_200_0deg.append(n_eff_200_0deg)
-            
-            a_400_15_deg_unscaled = ((self.unscaled_400_light[0][i] - self.unscaled_400_dark[0][i]) / (self.unscaled_400_light[5][i] - self.unscaled_400_dark[5][i])) ** 0.5
-            a_400_0_deg_unscaled = ((self.unscaled_400_light[1][i] - self.unscaled_400_dark[1][i]) / (self.unscaled_400_light[4][i] - self.unscaled_400_dark[4][i])) ** 0.5
 
-            n_eff_400_15deg_unscaled = ((-1 * (c - b + a_400_15_deg_unscaled * (c - b)) - ((c - b + a_400_15_deg_unscaled * (c - b)) ** 2 + (4 * b * c * ((1 - a_400_15_deg_unscaled) ** 2))) ** 0.5) / (2 * (1 - a_400_15_deg_unscaled)))
-            n_eff_400_0deg_unscaled = ((-1 * (c - b + a_400_0_deg_unscaled * (c - b)) - ((c - b + a_400_0_deg_unscaled * (c - b)) ** 2 + (4 * b * c * ((1 - a_400_0_deg_unscaled) ** 2))) ** 0.5) / (2 * (1 - a_400_0_deg_unscaled)))
-
-            self.n_eff_400_unscaled_15deg.append(n_eff_400_15deg_unscaled)
-            self.n_eff_400_unscaled_0deg.append(n_eff_400_0deg_unscaled)
-
-            a_200_15_deg_unscaled = ((self.unscaled_200_light[0][i] - self.unscaled_200_dark[0][i]) / (self.unscaled_200_light[5][i] - self.unscaled_200_dark[5][i])) ** 0.5
-            a_200_0_deg_unscaled = ((self.unscaled_200_light[1][i] - self.unscaled_200_dark[1][i]) / (self.unscaled_200_light[4][i] - self.unscaled_200_dark[4][i])) ** 0.5
-
-            n_eff_200_15deg_unscaled = ((-1 * (c - b + a_200_15_deg_unscaled * (c - b)) - ((c - b + a_200_15_deg_unscaled * (c - b)) ** 2 + (4 * b * c * ((1 - a_200_15_deg_unscaled) ** 2))) ** 0.5) / (2 * (1 - a_200_15_deg_unscaled)))
-            n_eff_200_0deg_unscaled = ((-1 * (c - b + a_200_0_deg_unscaled * (c - b)) - ((c - b + a_200_0_deg_unscaled * (c - b)) ** 2 + (4 * b * c * ((1 - a_200_0_deg_unscaled) ** 2))) ** 0.5) / (2 * (1 - a_200_0_deg_unscaled)))
-
-            self.n_eff_200_unscaled_15deg.append(n_eff_200_15deg_unscaled)
-            self.n_eff_200_unscaled_0deg.append(n_eff_200_0deg_unscaled)
-            
 data = DataGathering()
+data.background_correction()
 data.scale()
+data.sellmeier()
 data.effective_index()
-data.water_indices()
--- tests/plots.py
+++ tests/plots.py
@@ -3,11 +3,10 @@ import numpy as np
 from data_gathering import DataGathering
 
 data = DataGathering()
-data.scale()
 data.background_correction()
-data.effective_index_15deg()
-data.effective_index_0deg()
-data.test_effective_index_15deg()
+data.scale()
+data.sellmeier()
+data.effective_index()
 
 plt.rcParams.update({'font.size': 8})
 
@@ -47,19 +46,15 @@ axs1[1].set_title('200$\\mu$ fiber')
 plt.tight_layout()
 plt.margins(0)
 
-fig2, axs2 = plt.subplots(2, 2, figsize=(14, 8), facecolor='whitesmoke')
+fig2, axs2 = plt.subplots(2, 1, figsize=(14, 8), facecolor='whitesmoke')
 
-axs2[0,0].set_prop_cycle('color', plt.cm.jet(np.linspace(0, 1, 6)))
-axs2[0,1].set_prop_cycle('color', plt.cm.jet(np.linspace(0, 1, 6)))
-axs2[1,0].set_prop_cycle('color', plt.cm.jet(np.linspace(0, 1, 6)))
-axs2[1,1].set_prop_cycle('color', plt.cm.jet(np.linspace(0, 1, 6)))
+axs2[0].set_prop_cycle('color', plt.cm.jet(np.linspace(0, 1, 6)))
+axs2[1].set_prop_cycle('color', plt.cm.jet(np.linspace(0, 1, 6)))
 
-for i in range(len(data.scaled_400_light)):
-    
-    axs2[0,0].plot(data.wavelengths, data.scaled_400_light[i], label = f'{data.good_400[i]}')
-    axs2[0,1].plot(data.wavelengths, data.scaled_200_light[i], label = f'{data.good_200[i]}')
-    axs2[1,0].plot(data.wavelengths, data.unscaled_400_light[i], label = f'{data.good_400[i]}')
-    axs2[1,1].plot(data.wavelengths, data.unscaled_200_light[i], label = f'{data.good_200[i]}')
+for i in range(len(data.scaled_400)):
+
+    axs2[0].plot(data.wavelengths_400nm, data.scaled_400[i], label = f'{data.good_400[i]}')
+    axs2[1].plot(data.wavelengths_400nm, data.scaled_200[i], label = f'{data.good_200[i]}')
 
 for ax in axs2.flatten():
 
@@ -81,27 +76,21 @@ plt.margins(0)
 
 fig3, axs3 = plt.subplots(2, 2, figsize=(14, 8), facecolor='whitesmoke')
 
-axs3[0,0].plot(data.wavelengths_400nm, data.n_eff_positive_400_15deg, c='dodgerblue')
-axs3[0,1].plot(data.wavelengths_400nm, data.n_eff_negative_400_15deg, c='dodgerblue')
+axs3[0,0].plot(data.wavelengths_400nm, data.n_eff_400_15deg, c='dodgerblue', ls='dotted')
+axs3[0,1].plot(data.wavelengths_400nm, data.n_eff_400_0deg, c='dodgerblue', ls='dotted')
 
-axs3[1,0].plot(data.wavelengths_400nm, data.n_eff_positive_200_15deg, c='dodgerblue')
-axs3[1,1].plot(data.wavelengths_400nm, data.n_eff_negative_200_15deg, c='dodgerblue')
+axs3[1,0].plot(data.wavelengths_400nm, data.n_eff_200_15deg, c='dodgerblue', ls='dotted')
+axs3[1,1].plot(data.wavelengths_400nm, data.n_eff_200_0deg, c='dodgerblue', ls='dotted')
 
-# axs3[0,0].plot(data.wavelengths_400nm, data.n_eff_positive_400_unscaled_15deg, c='dodgerblue')
-# axs3[0,1].plot(data.wavelengths_400nm, data.n_eff_negative_400_unscaled_15deg, c='dodgerblue')
-
-# axs3[1,0].plot(data.wavelengths_400nm, data.n_eff_positive_200_unscaled_15deg, c='dodgerblue')
-# axs3[1,1].plot(data.wavelengths_400nm, data.n_eff_negative_200_unscaled_15deg, c='dodgerblue')
-
-major_ticks_x_3 = np.arange(400, 1150, 50)
-minor_ticks_x_3 = np.arange(400, 1150, 10)
+major_ticks_x_3 = np.arange(400, 950, 50)
+minor_ticks_x_3 = np.arange(400, 950, 10)
 # major_ticks_y_3 = np.arange(0, 75000, 5000)
 # minor_ticks_y_3 = np.arange(0, 75000, 1000)
 
 for ax in axs3.flatten():
 
     ax.set_xlabel('Wavelength [nm]')
-    ax.set_ylabel('Scope [ADC Counts]')
+    ax.set_ylabel('Calculated effectice index')
 
     ax.set_xticks(major_ticks_x_3)
     ax.set_xticks(minor_ticks_x_3, minor=True)
@@ -110,104 +99,39 @@ for ax in axs3.flatten():
 
     ax.grid(which='major', alpha=0.5, lw=.8, ls='--')
     ax.grid(which='minor', alpha=0.3, lw=.6, ls='--')
-    ax.set_xlim(400, 1100)
-    # ax.set_ylim(0, 70000)
-    ax.set_facecolor('dimgrey')
-
-axs3[0,0].title.set_text('$+n_{eff}$  $400\\mu$')
-axs3[0,1].title.set_text('$-n_{eff}$  $400\\mu$')
-axs3[1,0].title.set_text('$+n_{eff}$  $200\\mu$')
-axs3[1,1].title.set_text('$-n_{eff}$  $200\\mu$')
-
-fig3.suptitle(' Scaled 15$\\degree$ ', fontsize=20)
-plt.tight_layout()
-plt.margins(0)
-
-fig4, axs4 = plt.subplots(2, 2, figsize=(14, 8), facecolor='whitesmoke')
-
-axs4[0,0].plot(data.wavelengths_400nm, data.n_eff_positive_400_0deg, c='dodgerblue')
-axs4[0,1].plot(data.wavelengths_400nm, data.n_eff_negative_400_0deg, c='dodgerblue')
-
-axs4[1,0].plot(data.wavelengths_400nm, data.n_eff_positive_200_0deg, c='dodgerblue')
-axs4[1,1].plot(data.wavelengths_400nm, data.n_eff_negative_200_0deg, c='dodgerblue')
-
-# axs4[0,0].plot(data.wavelengths_400nm, data.n_eff_positive_400_unscaled_0deg, c='dodgerblue')
-# axs4[0,1].plot(data.wavelengths_400nm, data.n_eff_negative_400_unscaled_0deg, c='dodgerblue')
-
-# axs4[1,0].plot(data.wavelengths_400nm, data.n_eff_positive_200_unscaled_0deg, c='dodgerblue')
-# axs4[1,1].plot(data.wavelengths_400nm, data.n_eff_negative_200_unscaled_0deg, c='dodgerblue')
-
-major_ticks_x_4 = np.arange(400, 1150, 50)
-minor_ticks_x_4 = np.arange(400, 1150, 10)
-# major_ticks_y_4 = np.arange(0, 75000, 5000)
-# minor_ticks_y_4 = np.arange(0, 75000, 1000)
-
-for ax in axs4.flatten():
-
-    ax.set_xlabel('Wavelength [nm]')
-    ax.set_ylabel('Scope [ADC Counts]')
-
-    ax.set_xticks(major_ticks_x_4)
-    ax.set_xticks(minor_ticks_x_4, minor=True)
-    # ax.set_yticks(major_ticks_y_4)
-    # ax.set_yticks(minor_ticks_y_4, minor=True)
-
-    ax.grid(which='major', alpha=0.5, lw=.8, ls='--')
-    ax.grid(which='minor', alpha=0.3, lw=.6, ls='--')
-    ax.set_xlim(400, 1100)
-    # ax.set_ylim(0, 70000)
-    ax.set_facecolor('dimgrey')
-
-axs4[0,0].title.set_text('$+n_{eff}$  $400\\mu$')
-axs4[0,1].title.set_text('$-n_{eff}$  $400\\mu$')
-axs4[1,0].title.set_text('$+n_{eff}$  $200\\mu$')
-axs4[1,1].title.set_text('$-n_{eff}$  $200\\mu$')
-
-fig4.suptitle(' Scaled 0$\\degree$ ', fontsize=20)
-plt.tight_layout()
-plt.margins(0)
-
-fig5, axs5 = plt.subplots(2, 2, figsize=(14, 8), facecolor='whitesmoke')
-
-axs5[0,0].plot(data.wavelengths_400nm, data.n_eff_400_15deg_test, c='dodgerblue')
-axs5[0,1].plot(data.wavelengths_400nm, data.n_eff_400_0deg_test, c='dodgerblue')
-
-axs5[1,0].plot(data.wavelengths_400nm, data.n_eff_200_15deg_test, c='dodgerblue')
-axs5[1,1].plot(data.wavelengths_400nm, data.n_eff_200_0deg_test, c='dodgerblue')
-
-# axs5[0,0].plot(data.wavelengths_400nm, data.n_eff_400_unscaled_15deg_test, c='dodgerblue')
-# axs5[0,1].plot(data.wavelengths_400nm, data.n_eff_400_unscaled_15deg_test, c='dodgerblue')
-
-# axs5[1,0].plot(data.wavelengths_400nm, data.n_eff_200_unscaled_15deg_test, c='dodgerblue')
-# axs5[1,1].plot(data.wavelengths_400nm, data.n_eff_200_unscaled_15deg_test, c='dodgerblue')
-
-major_ticks_x_5 = np.arange(400, 950, 50)
-minor_ticks_x_5 = np.arange(400, 950, 10)
-major_ticks_y_5 = np.arange(1.3, 1.65 , 0.05)
-minor_ticks_y_5 = np.arange(1.3, 1.65, 0.01)
-
-for ax in axs5.flatten():
-
-    ax.set_xlabel('Wavelength [nm]')
-    ax.set_ylabel('$n_{eff}$')
-
-    ax.set_xticks(major_ticks_x_5)
-    ax.set_xticks(minor_ticks_x_5, minor=True)
-    ax.set_yticks(major_ticks_y_5)
-    ax.set_yticks(minor_ticks_y_5, minor=True)
-
-    ax.grid(which='major', alpha=0.5, lw=.8, ls='--')
-    ax.grid(which='minor', alpha=0.3, lw=.6, ls='--')
     ax.set_xlim(400, 900)
-    ax.set_ylim(1.3, 1.6)
+    # ax.set_ylim(0, 70000)
     ax.set_facecolor('dimgrey')
 
-axs5[0,0].title.set_text('$15\\degree$ $400\\mu$')
-axs5[0,1].title.set_text('$0\\degree$ $400\\mu$')
-axs5[1,0].title.set_text('$15\\degree$ $200\\mu$')
-axs5[1,1].title.set_text('$0\\degree$ $200\\mu$')
+axs3[0,0].title.set_text('$15\\degree$ $400\\mu$')
+axs3[0,1].title.set_text('$0\\degree$ $400\\mu$')
+axs3[1,0].title.set_text('$15\\degree$ $200\\mu$')
+axs3[1,1].title.set_text('$0\\degree$ $200\\mu$')
 
-fig5.suptitle(' Scaled 15$\\degree$ ', fontsize=20)
+fig3.suptitle(' Test Measurements ', fontsize=20)
+plt.tight_layout()
+plt.margins(0)
+
+fig4, axs4 = plt.subplots(1, figsize=(12, 8), facecolor='whitesmoke')
+
+axs4.plot(data.wavelengths_400nm, data.water_indices, c='dodgerblue')
+
+major_ticks_x_4 = np.arange(400, 950, 50)
+minor_ticks_x_4 = np.arange(400, 950, 10)
+major_ticks_y_4 = np.arange(1.3, 1.4, 0.005)
+minor_ticks_y_4 = np.arange(1.3, 1.4, 0.001)
+
+axs4.set_xticks(major_ticks_x_4)
+axs4.set_xticks(minor_ticks_x_4, minor=True)
+axs4.set_yticks(major_ticks_y_4)
+axs4.set_yticks(minor_ticks_y_4, minor=True)
+axs4.grid(which='major', alpha=0.5, lw=.8, ls='--')
+axs4.grid(which='minor', alpha=0.3, lw=.6, ls='--')
+axs4.set_xlim(400, 900)
+axs4.set_xlabel('Wavelength [nm]')
+axs4.set_ylabel('Refractive index')
+axs4.set_facecolor('dimgrey')
+
 plt.tight_layout()
 plt.margins(0)
 
@@ -215,6 +139,5 @@ plt.close(fig1)
 plt.close(fig2)
 plt.close(fig3)
 plt.close(fig4)
-# plt.close(fig5)
 
 plt.show()
