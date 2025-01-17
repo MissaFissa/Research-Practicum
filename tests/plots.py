848470d39ac912462956e6184d1c625488152b3f
MissaFissa <meho11@outlook.com>
2025-01-17 15:47:19 +0100

    plot update

--- tests/plots.py
+++ tests/plots.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt 
 import numpy as np
-from data_gathering import DataGathering
+from data_gathering import DataTests, DataExperiment
 
-data = DataGathering()
+data = DataTests()
 data.background_correction()
 data.scale()
-# data.sellmeier()
+data.sellmeier()
 data.effective_index()
+
+experiment = DataExperiment()
+experiment.background_correction()
+experiment.scale()
+experiment.sellmeier()
 
 plt.rcParams.update({'font.size': 8})
 
@@ -135,9 +140,48 @@ axs4.set_facecolor('dimgrey')
 plt.tight_layout()
 plt.margins(0)
 
+fig5, axs5 = plt.subplots(2, 2, figsize=(14, 8), facecolor='whitesmoke')
+
+axs5[0,0].plot(experiment.wavelengths_400nm, experiment.scaled_400_experiment[0], c='dodgerblue')
+axs5[0,1].plot(experiment.wavelengths_400nm, experiment.scaled_400_experiment[1], c='dodgerblue')
+
+axs5[1,0].plot(experiment.wavelengths_400nm, experiment.scaled_400_experiment[2], c='dodgerblue')
+axs5[1,1].plot(experiment.wavelengths_400nm, experiment.scaled_400_experiment[3], c='dodgerblue')
+
+major_ticks_x_5 = np.arange(400, 950, 50)
+minor_ticks_x_5 = np.arange(400, 950, 10)
+major_ticks_y_5 = np.arange(0, 75000, 5000)
+minor_ticks_y_5 = np.arange(0, 75000, 1000)
+
+for ax in axs5.flatten():
+
+    ax.set_xlabel('Wavelength [nm]')
+    ax.set_ylabel('Scope [ADC Counts]')
+
+    ax.set_xticks(major_ticks_x_5)
+    ax.set_xticks(minor_ticks_x_5, minor=True)
+    # ax.set_yticks(major_ticks_y_5)
+    # ax.set_yticks(minor_ticks_y_5, minor=True)
+
+    ax.grid(which='major', alpha=0.5, lw=.8, ls='--')
+    ax.grid(which='minor', alpha=0.3, lw=.6, ls='--')
+    ax.set_xlim(400, 900)
+    # ax.set_ylim(0, 70000)
+    ax.set_facecolor('dimgrey')
+
+axs5[0,0].title.set_text('Fat tissue $15\\degree$ $400\\mu$')
+axs5[0,1].title.set_text('Muscle tissue $15\\degree$ $400\\mu$')
+axs5[1,0].title.set_text('Red muscle tissue $15\\degree$ $200\\mu$')
+axs5[1,1].title.set_text('Skin tissue $15\\degree$ $200\\mu$')
+
+fig5.suptitle(' Experiment Measurements ', fontsize=20)
+plt.tight_layout()
+plt.margins(0)
+
 plt.close(fig1)
 plt.close(fig2)
 plt.close(fig3)
 plt.close(fig4)
+plt.close(fig5)
 
 plt.show()
